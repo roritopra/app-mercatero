58f68da2a737cd59be9f268e0e4cd15f13eeb25c
roritopra <1193102598@u.icesi.edu.co>
2023-11-30 20:55:50 -0500

    [ADD] Added XML styles from local

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "icesi.edu.co.mercatero_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "icesi.edu.co.mercatero_app"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +33,33 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding=true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val navVersion = "2.7.5"
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
+
+    //View pager dots indicator
+    implementation("com.tbuonomo:dotsindicator:5.0")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation ("com.github.ibrahimsn98:SmoothBottomBar:1.7.9")
+
+
 }
